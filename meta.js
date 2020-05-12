d72782839b99008202d58667ba033c26436717be
WonKwangYeon <pangyeon@lotte.net>
2020-05-12 14:54:15 +0900

    autoinstall 수정

--- meta.js
+++ meta.js
@@ -40,8 +40,27 @@ module.exports = {
       fontAwesome: {
           type: 'confirm',
           message : 'Install font-awesome?'
+      },
+      autoInstall: {
+        type: 'list',
+        message: 'Should we run `npm install` for you after the project has been created? (recommended)',
+        choices: [{
+          name: 'Yes, use NPM',
+          value: 'npm',
+          short: 'npm',
+        },
+          {
+            name: 'Yes, use Yarn',
+            value: 'yarn',
+            short: 'yarn',
+          },
+          {
+            name: 'No, I will handle that myself',
+            value: false,
+            short: 'no',
+          },
+        ]
       }
-    
     },
   
     /**
--- template/src/main.js
+++ template/src/main.js
@@ -2,18 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index"
 import store from "./store"
-// import { BootstrapVue } from 'bootstrap-vue'
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
-// import { library } from "@fortawesome/fontawesome-svg-core"
-// import {  } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+// import { BootstrapVue } from 'bootstrap-vue'   // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// import 'bootstrap/dist/css/bootstrap.css'      // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// import 'bootstrap-vue/dist/bootstrap-vue.css'  // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// import { library } from "@fortawesome/fontawesome-svg-core"    // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// import {  } from '@fortawesome/free-solid-svg-icons'           // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome" // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 
 Vue.config.productionTip = false
-// Vue.use(BootstrapVue)
-// Vue.use(require('vue-moment'));
-// library.add(faChevronLeft)
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.use(BootstrapVue)  // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거          
+// Vue.use(require('vue-moment'));  // moment 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// library.add(faChevronLeft)                           // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+// Vue.component('font-awesome-icon', FontAwesomeIcon)  // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 
 new Vue({
   render: h => h(App),
