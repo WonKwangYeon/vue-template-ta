a9f235b6eedc5e0f01a16e5aa532f318b6283b53
WonKwangYeon <pangyeon@lotte.net>
2020-05-13 13:46:35 +0900

    fontawesome, moment 선택안할 시 main.js import 제거

--- template/src/main.js
+++ template/src/main.js
@@ -3,20 +3,27 @@ import App from './App.vue'
 import router from "./router/index"
 import store from "./store"
 {{#bootstrap}}
-// import { BootstrapVue } from 'bootstrap-vue'   // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// import 'bootstrap/dist/css/bootstrap.css'      // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// import 'bootstrap-vue/dist/bootstrap-vue.css'  // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+import { BootstrapVue } from 'bootstrap-vue'  
+import 'bootstrap/dist/css/bootstrap.css'     
+import 'bootstrap-vue/dist/bootstrap-vue.css' 
 {{/bootstrap}}
-// import { library } from "@fortawesome/fontawesome-svg-core"    // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// import {  } from '@fortawesome/free-solid-svg-icons'           // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome" // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+{{#fontAwesome}}
+import { library } from "@fortawesome/fontawesome-svg-core"   
+import {  } from '@fortawesome/free-solid-svg-icons'           
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome" 
+{{/fontAwesome}}
 
 Vue.config.productionTip = false
-// Vue.use(BootstrapVue)  // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거          
-// Vue.use(require('vue-moment'));  // moment 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// library.add(faChevronLeft)                           // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-// Vue.component('font-awesome-icon', FontAwesomeIcon)  // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
-
+{{#bootstrap}}
+Vue.use(BootstrapVue)
+{{/bootstrap}}
+{{#moment}}
+Vue.use(require('vue-moment'));
+{{/moment}}
+{{#fontAwesome}}
+library.add(faChevronLeft)                          
+Vue.component('font-awesome-icon', FontAwesomeIcon) 
+{{/fontAwesome}}
 new Vue({
   render: h => h(App),
   router,
