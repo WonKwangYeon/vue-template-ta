b022bf0ff655577358cb9654ab025f213b007b8a
WonKwangYeon <pangyeon@lotte.net>
2020-05-13 13:42:16 +0900

    bootstrap 선택시 main.js에서 import 지우는 template 수정

--- template/src/main.js
+++ template/src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index"
 import store from "./store"
+{{#bootstrap}}
 // import { BootstrapVue } from 'bootstrap-vue'   // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 // import 'bootstrap/dist/css/bootstrap.css'      // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 // import 'bootstrap-vue/dist/bootstrap-vue.css'  // bootstrap 사용한다면 주석 제거 사용하지 않는다면 코드 제거
+{{/bootstrap}}
 // import { library } from "@fortawesome/fontawesome-svg-core"    // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 // import {  } from '@fortawesome/free-solid-svg-icons'           // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
 // import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome" // fontAwsome 사용한다면 주석 제거 사용하지 않는다면 코드 제거
